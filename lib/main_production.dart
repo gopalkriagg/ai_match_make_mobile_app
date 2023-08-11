c19e4a0d0bcbef80d6b8931807cb1d924c3e3194
Gopal Krishan Aggarwal <gopalkriagg@gmail.com>
2023-08-11 21:54:23 +0900

    Initialize very_good flutter project

---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:ai_match_make_mobile_app/app/app.dart';
+import 'package:ai_match_make_mobile_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:ai_match_make_mobile_app/app/app.dart';
+import 'package:ai_match_make_mobile_app/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
